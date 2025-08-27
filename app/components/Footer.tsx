bbf8969422682eafb49b5f401f76cf47b9f50f0a
Aaron Chen <acaaron@bu.edu>
2025-08-26 22:28:40 -0400

    Footer fix

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,8 +1,10 @@
+import Link from "next/link";
+
 export default function Footer() {
     return (
         <footer className="flex justify-center items-center bg-black text-white text-lg font-semibold">
             <p>
-                All Rights Reserved by Aaron Chen &nbsp;|&nbsp; Credits &copy;
+                All Rights Reserved by Aaron Chen&nbsp;|&nbsp;<Link href={`/`}>Credits</Link> &copy;
             </p>
         </footer>
     );
